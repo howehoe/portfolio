fa10c79ed8be16bc3e99f3a095775f7e4e8ddc95
howehoe <98229430+howehoe@users.noreply.github.com>
2022-01-27 21:58:39 +0900

    Add files via upload

--- TO MOON/assets/js/index.js
+++ TO MOON/assets/js/index.js
@@ -61,29 +61,26 @@ $(".rocket").click(function() {
    if (this.id == "rocket1") centerrocket1();
    if (this.id == "rocket2") centerrocket2();
    if (this.id == "rocket3") centerrocket3();
-
 });
-
-
 function centerrocket1() { 
    var tl = new TimelineMax()
    .to('#rocket1', 1, {xPercent: 0, z: 1}, 0)
-   .to('#rocket2', 1, {xPercent: 140, z: -800}, 0)
-   .to('#rocket3', 1, {xPercent: -140, z: -800}, 0)
+   .to('#rocket2', 1, {xPercent: 100, z: -1000}, 0)
+   .to('#rocket3', 1, {xPercent: -100, z: -1000}, 0)
    
 }
 
 function centerrocket2() {
    var tl = new TimelineMax()
-   .to('#rocket1', 1, {xPercent: -140, z: -800}, 0)
+   .to('#rocket1', 1, {xPercent: -100, z: -1000}, 0)
    .to('#rocket2', 1, {xPercent: 0, z: 1}, 0)
-   .to('#rocket3', 1, {xPercent: 140, z: -800}, 0);
+   .to('#rocket3', 1, {xPercent: 100, z: -1000}, 0);
 }
 
 function centerrocket3() {
    var tl = new TimelineMax()
-   .to('#rocket1', 1, {xPercent: 140, z: -800}, 0)
-   .to('#rocket2', 1, {xPercent: -140, z: -800}, 0)
+   .to('#rocket1', 1, {xPercent: 100, z: -1000}, 0)
+   .to('#rocket2', 1, {xPercent: -100, z: -1000}, 0)
    .to('#rocket3', 1, {xPercent: 0, z: 1}, 0);
 }
 
