d4df7973de6348c4fe8460414baa9fcb0de5bb5b
howehoe <98229430+howehoe@users.noreply.github.com>
2022-02-03 23:00:35 +0900

    Add files via upload

--- TO_MOON/assets/js/index.js
+++ TO_MOON/assets/js/index.js
@@ -86,20 +86,20 @@ $(".rocket").click(function() {
 function centerrocket1() { 
   var tl = new TimelineMax()
   .to('#rocket1', 1, {xPercent: 0, z: 1,opacity: '1'}, 0)
-  .to('#rocket2', 1, {xPercent: 140, z:-600 , opacity: '0.6'}, 0)
-  .to('#rocket3', 1, {xPercent: -140, z: -600 , opacity: '0.6'}, 0)
+  .to('#rocket2', 1, {xPercent: 140, z:-800 , opacity: '0.6'}, 0)
+  .to('#rocket3', 1, {xPercent: -140, z: -800 , opacity: '0.6'}, 0)
 }
 
 function centerrocket2() {
   var tl = new TimelineMax()
-  .to('#rocket1', 1, {xPercent: -140, z: -600 , opacity: '0.6'}, 0)
+  .to('#rocket1', 1, {xPercent: -140, z: -800 , opacity: '0.6'}, 0)
   .to('#rocket2', 1, {xPercent: 0, z: 1 , opacity: '1'}, 0)
-  .to('#rocket3', 1, {xPercent: 140, z: -600 , opacity: '0.6'}, 0);
+  .to('#rocket3', 1, {xPercent: 140, z: -800 , opacity: '0.6'}, 0);
 }
 function centerrocket3() {
   var tl = new TimelineMax()
-  .to('#rocket1', 1, {xPercent: 140, z: -600 , opacity: '0.6'}, 0)
-  .to('#rocket2', 1, {xPercent: -140, z: -600 , opacity: '0.6'}, 0)
+  .to('#rocket1', 1, {xPercent: 140, z: -800 , opacity: '0.6'}, 0)
+  .to('#rocket2', 1, {xPercent: -140, z: -800 , opacity: '0.6'}, 0)
   .to('#rocket3', 1, {xPercent: 0, z: 1 , opacity: '1'}, 0);
 }
 
